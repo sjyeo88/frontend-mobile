6ed3a2b457ee42eeaa0300f1ce1e659306223b74
sjyeo88 <sjyeo88@gmail.com>
2017-10-12 03:40:29 +0900

    Ionic View Test12

--- src/configure/config.ts
+++ src/configure/config.ts
@@ -3,6 +3,6 @@ export class AppConfig {
   backServerHost:string
   constructor(){
     this.password = 'test'
-    this.backServerHost = 'http://192.168.1.3:8080'
+    this.backServerHost = 'http://182.162.104.243:3000'
   }
 }
